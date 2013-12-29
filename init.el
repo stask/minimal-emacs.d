19bf456bc6629c8ad732af18368a5ca673865467
stask <stask@me.com>
2013-12-29 18:17:07 +0200

    random clojure/repl related stuff

--- init.el
+++ init.el
@@ -104,13 +104,33 @@
                     (volatile)
                     (headline "^[;(]")))))
 
+;; cider-reload
+(require 'clojure-mode)
+(defun cider-namespace-refresh ()
+  "Uses Stuart Sierra's clojure.tools.namespace to reload all changed namespaces."
+  (interactive)
+  (cider-interactive-eval
+   "(require 'clojure.tools.namespace.repl)
+    (clojure.tools.namespace.repl/refresh)"))
+
+(defun cider-project-reset ()
+  "Uses Stuart Sierra's workflow to reload and restart Clojure application."
+  (interactive)
+  (cider-interactive-eval "(user/reset)"))
+
+(defun reindent-whole-buffer ()
+  "Reindent the whole buffer."
+  (interactive)
+  (indent-region (point-min)
+                 (point-max)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
- '(clojure-defun-indents (quote (context GET POST with-db wcar cond->)))
+ '(clojure-defun-indents (quote (context GET POST DELETE with-db wcar cond->)))
  '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(global-whitespace-mode t)
